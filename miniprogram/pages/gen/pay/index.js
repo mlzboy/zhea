2b0be3b5fc977a8aed1cdf3fda590de59c0267d5
mlzboy <13426444511@126.com>
2020-05-23 15:53:46 +0800

    完成 图片下载功能

--- miniprogram/pages/gen/pay/index.js
+++ miniprogram/pages/gen/pay/index.js
@@ -74,6 +74,38 @@ copy:function(e){
     }
   })
 },
+down: function(e){　　　　　　　　　　　　　　　　//触发函数
+  // console.log(e.currentTarget.dataset.url)
+  wx.downloadFile({
+    url: "https://cloud-minapp-23060.cloud.ifanrusercontent.com/1jcOlP3TIinB7HCU.png",　　　　　　　//需要下载的图片url
+    success: function (res) {　　　　　　　　　　　　//成功后的回调函数
+      wx.saveImageToPhotosAlbum({　　　　　　　　　//保存到本地
+        filePath: res.tempFilePath,
+        success(res) {
+          wx.showToast({
+            title: '保存成功',
+            icon: 'success',
+            duration: 2000
+          })
+        },
+        fail: function (err) {
+          if (err.errMsg === "saveImageToPhotosAlbum:fail auth deny") {
+            wx.openSetting({
+              success(settingdata) {
+                console.log(settingdata)
+                if (settingdata.authSetting['scope.writePhotosAlbum']) {
+                  console.log('获取权限成功，给出再次点击图片保存到相册的提示。')
+                } else {
+                  console.log('获取权限失败，给出不给权限就无法正常使用的提示')
+                }
+              }
+            })
+          }
+        }
+      })
+    }
+  });
+},
   /**
    * 用户点击右上角分享
    */
